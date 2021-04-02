916e6e95ff2d09efa67e5a78fe2de872e52b0c07
Eston Kagwima <ekagwima745@gmail.com>
2021-04-02 03:58:31 +0300

    initial commit adding navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import{LandingComponent} from './landing-page/landing/landing.component';
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
